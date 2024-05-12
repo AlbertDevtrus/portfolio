d72254aa7b4ff50dd423a345ba78d4d629253ccf
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-05-12 15:51:10 -0500

    Fix line-projects responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,9 +19,6 @@ function About() {
         const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
         const isWebGL2 = gl && typeof gl.getParameter === 'function' && gl.getParameter(gl.VERSION).includes('WebGL 2.0');
         setIsWebGL2Supported(isWebGL2);
-
-        console.log(gl.VERSION)
-
     }, []);
 
     const paragraph = "My name is Luis and I'm studying web Development. My focus is on front-end technologies such as HTML, CSS, JavaScript, React, and Framer Motion, I have a strong interest in delving into the world of UX/UI design for the web. I'm eager to collaborate on interesting projects and bring creative ideas to life!"

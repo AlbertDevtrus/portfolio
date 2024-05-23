873feb0101b5364c0dd027aa9edaa95c4e7eecde
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-05-23 11:47:13 -0500

    fix smooth scroll

--- src/hooks/useSmoothScroll.js
+++ src/hooks/useSmoothScroll.js
@@ -1,7 +1,12 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
 export default function useSmoothScroll() {
-    const locomotiveScroll = new LocomotiveScroll();
+    const locomotiveScroll = new LocomotiveScroll({
+        lenisOptions: {
+            lerp: 0.05,
+            wheelMultiplier: 0.5,
+        }
+    });
 
     return { locomotiveScroll };
 }
